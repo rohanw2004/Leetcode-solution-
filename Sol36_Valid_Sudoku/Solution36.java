24abbd0f27d42bf67e56462700ef5ab94ed33216
Rohan Wajekar <rohanswajekar2004@gmail.com>
2026-09-22 00:16:33 +0530

    Add files via upload

---
+++ Sol36_Valid_Sudoku/Solution36.java
@@ -0,0 +1,70 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] boxes = new HashSet[9];
+
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
+
+                int boxIdx = (i / 3) * 3 + (j / 3);
+
+                if (rows[i].contains(c) || cols[j].contains(c) || boxes[boxIdx].contains(c)) {
+                    return false;
+                }
+
+                rows[i].add(c);
+                cols[j].add(c);
+                boxes[boxIdx].add(c);
+            }
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+
+        // Example 1
+        char[][] board1 = {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        // Example 2
+        char[][] board2 = {
+            {'8','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+
+
+        Solution solution = new Solution();
+
+        System.out.println("Example 1 Output: " + solution.isValidSudoku(board1));
+        System.out.println("Example 2 Output: " + solution.isValidSudoku(board2));
+    }
+}
